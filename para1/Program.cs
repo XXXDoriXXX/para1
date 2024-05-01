d8b452c18e066c2c7f947e64149030cc6e294e9a
vadym <vadym@DORI>
2024-05-01 20:52:21 +0300

    Добавьте файлы проекта.

---
+++ para1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введіть перше дійсне число:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге дійсне число:");
+            double y = double.Parse(Console.ReadLine());
+
+            double sum = x + y;
+            double product = x * y;
+            double arithmeticMean = (x + y) / 2;
+            double geometricMean = Math.Sqrt(x * y);
+
+            Console.WriteLine("Сума: " + sum);
+            Console.WriteLine("Добуток: " + product);
+            Console.WriteLine("Середнє арифметичне: " + arithmeticMean);
+            Console.WriteLine("Середнє геометричне: " + geometricMean);
+
+        }
+    }
+
+}
+
+
